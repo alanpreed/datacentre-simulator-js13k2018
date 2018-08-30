ceac27a138df95fa2f1db36656396851c58c6814
Alan Reed <alan_p_reed@hotmail.co.uk>
2018-08-30 22:18:02 +0100

    Changed blocks to be drawn relative to their centre

--- src/block.js
+++ src/block.js
@@ -12,11 +12,10 @@ export class Block {
       this.context.translate(this.x, this.y);
       this.context.rotate(this.rotation);
       this.context.fillStyle = this.color;
-      this.context.fillRect(0, 0, this.width, this.height);
+      this.context.fillRect(-this.width/2, -this.height/2, this.width, this.height);
       this.context.restore();
     }
   }
-
   update(){
     this.lifetime -= 1;
   }
