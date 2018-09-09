a657d244eed6776d70f29b50427e4f0da961aef9
Alan Reed <alan_p_reed@hotmail.co.uk>
2018-09-09 22:57:18 +0100

    Changed packets to spawn from all sides

--- src/packetSpawner.js
+++ src/packetSpawner.js
@@ -7,13 +7,36 @@ export class packetSpawner {
     this.canvasHeight = canvasHeight;
     this.packetWidth = 4;
 
-    this.x = -this.packetWidth;
-    this.y = this.packetWidth + (Math.random() * (this.canvasHeight - (2 * this.packetWidth)));
-    if(this.y < this.canvasHeight / 2) {
-      this.rotation = Math.random() * Math.PI / 2
+    let side = Math.round(Math.random() * 3);
+
+    switch (side) {
+      case 0:
+        this.x = -this.packetWidth;
+        this.y = this.packetWidth + (Math.random() * (this.canvasHeight - (2 * this.packetWidth)));
+        break;
+      case 1:
+        this.x = this.packetWidth + (Math.random() * (this.canvasWidth - (2 * this.packetWidth)))
+        this.y = -this.packetWidth;
+        break;
+      case 2:
+        this.x = this.canvasWidth + this.packetWidth;
+        this.y = this.packetWidth + (Math.random() * (this.canvasHeight - (2 * this.packetWidth)));
+        break;
+      case 3:
+        this.x = this.packetWidth + (Math.random() * (this.canvasWidth - (2 * this.packetWidth)))
+        this.y = this.canvasHeight + this.packetWidth;
+        break;
+      default:
+    }
+
+    if(this.x < this.canvasWidth / 2) {
+      this.rotation = (Math.random() * Math.PI / 2);
     }
     else {
-      this.rotation = -Math.random() * Math.PI / 2
+      this.rotation = (Math.PI / 2) + (Math.random() * Math.PI / 2);
+    }
+    if(this.y > this.canvasHeight / 2) {
+      this.rotation *= -1;
     }
   }
 
